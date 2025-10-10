9fb67b6736648897c47a8a808daa8b9fb78ac635
arvinvahed <52027097+arvinvahed@users.noreply.github.com>
2025-10-10 23:02:47 +0330

    Final Fix(Factory): Manually create Faker instance to guarantee seeding success

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,9 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+
+        $faker = Faker::create();
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
