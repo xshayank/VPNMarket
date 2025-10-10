2f6bb0e9147d4f2437d84196b42925f6e1c9a363
arvinvahed <52027097+arvinvahed@users.noreply.github.com>
2025-10-10 23:12:46 +0330

    Final Fix(Seeder): Bypass UserFactory and create static admin in DatabaseSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -24,12 +23,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        // ساخت دستی نمونه Faker
-        $faker = Faker::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
